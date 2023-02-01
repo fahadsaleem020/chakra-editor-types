22c2ea910ec0623b74b28daca3e23d800e94143b
palari <fahadsaleem020@gmail.com>
2023-02-01 18:27:10 +0500

    perf: v1.0.0
    initial release

---
+++ rollup.config.mjs
@@ -0,0 +1,19 @@
+import typescript from "rollup-plugin-typescript2";
+import { defineConfig } from "rollup";
+
+export default defineConfig({
+  input: "index.ts",
+  output: {
+    file: "lib/index.esm.js",
+    format: "esm",
+  },
+
+  plugins: [
+    typescript({
+      tsconfig: "./tsconfig.json",
+      include: ["index.ts"],
+      exclude: ["src"],
+    }),
+  ],
+  external: ["@chakra-ui/react", "@tiptap/react", "react"],
+});
